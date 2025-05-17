61f57ac7578bd4f1ccdd11d84347b507c364fc24
Rishi Tiwari <rishi008.tiwari@gmail.com>
2025-05-17 22:47:58 +0530

    fetch scan history DynamoDB

---
+++ Lambdas/scanHistory/index.mjs
@@ -0,0 +1,37 @@
+import { DynamoDBDocumentClient,QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.tableName || "SpringPokedex";
+
+const createResponse = (statusCode, body) => {
+    const responseBody = JSON.stringify(body);
+    return {
+        statusCode,
+        headers: { "Content-Type": "application/json" },
+        body: responseBody,
+    };
+};
+
+export const handler = async (event) => {
+    const userSub = event?.requestContext?.authorizer?.jwt?.claims?.sub;
+    try {
+        let command = new QueryCommand({
+            TableName: tableName,
+            KeyConditionExpression: "userId = :uid",
+            ExpressionAttributeValues: {
+                ":uid": userSub,
+            },
+        });
+
+        const response = await docClient.send(command);
+        console.log(response);
+        return createResponse(200, response);
+    }
+    catch (err) {
+        console.error("Error fetching data from DynamoDB:", err);
+        return createResponse(500, { error: err.message });
+    }
+}
